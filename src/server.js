d1d50023ab634d5066adf2b107c2b422b4683fc3
Nikola Tasic <ntasic7@gmail.com>
2020-01-14 21:06:28 +0100

    rm dist

--- src/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var server = express_1.default();
-var PORT = parseInt(process.env.PORT) || 3000;
