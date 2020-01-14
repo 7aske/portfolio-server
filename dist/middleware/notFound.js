c2fb7acf8138927cd108df181c68162a1bf8331b
Nikola Tasic <ntasic7@gmail.com>
2020-01-14 22:22:33 +0100

    rm dist

--- dist/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = require("path");
-function notFound(req, res, next) {
-    res.status(404).sendFile(path_1.resolve(process.cwd(), "dist/static/pages/404.html"));
-}
-exports.notFound = notFound;
